3d1ba622640f5f3329278ed58f06b314d0c172dd
biel965 <bieleugenio2005@gmail.com>
2025-02-16 16:53:23 -0300

    test commit

---
+++ mod9/src/mod9/Main.java
@@ -0,0 +1,14 @@
+package mod9;
+
+public class Main {
+	public static void main(String args[]) {
+		int num1 = 10;
+		int num2 = 35;
+		int num3 = 55;
+		int num4 = 22;
+		
+		int soma = num1 + num2 + num3 + num4;
+		int media = soma / 4;
+		System.out.println("A soma dos numeros é: " + soma + " e a media é: " + media);
+	}
+}
